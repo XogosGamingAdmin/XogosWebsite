dfcc2e8897cdbb7774a128b9743ec32a67d90d7c
XogosGamingAdmin <myxogos@gmail.com>
2025-12-18 13:44:07 -0600

    Fix Prettier formatting in board-signin page

--- app/board-signin/page.tsx
+++ app/board-signin/page.tsx
@@ -6,11 +6,14 @@ import styles from "../signin/signin.module.css";
 export default function BoardSignInPage() {
   useEffect(() => {
     // Get Google OAuth credentials from the page
-    const clientId = "1020785079037-7921ohj4rp89cjtu7msslljhfa6r7ag4.apps.googleusercontent.com";
+    const clientId =
+      "1020785079037-7921ohj4rp89cjtu7msslljhfa6r7ag4.apps.googleusercontent.com";
     const redirectUri = "https://www.histronics.com/api/auth/callback/google";
 
     // Construct Google OAuth URL
-    const googleAuthUrl = new URL("https://accounts.google.com/o/oauth2/v2/auth");
+    const googleAuthUrl = new URL(
+      "https://accounts.google.com/o/oauth2/v2/auth"
+    );
     googleAuthUrl.searchParams.set("client_id", clientId);
     googleAuthUrl.searchParams.set("redirect_uri", redirectUri);
     googleAuthUrl.searchParams.set("response_type", "code");
